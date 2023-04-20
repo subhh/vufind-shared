bde20a955e10d821d8628821ea3a5d7841c24c2e
David Maus <dmaus@dmaus.name>
2023-04-20 14:50:33 +0200

    Add OpenAccessViewHelper

---
+++ src/main/php/OpenAccessViewHelper.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+ * This file is part of VuFind Shared Components.
+ *
+ * VuFind Shared Components is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * VuFind Shared Components is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VuFind Shared Components. If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @author    David Maus <david.maus@sub.uni-hamburg.de>
+ * @copyright (c) 2023 by Staats- und Universitätsbibliothek Hamburg
+ * @license   http://www.gnu.org/licenses/gpl.txt GNU General Public License v3 or higher
+ */
+
+declare(strict_types=1);
+
+namespace SUBHH\VuFind\Shared;
+
+use SUBHH\VuFind\SolrMarc\SolrMarc;
+
+final class OpenAccessViewHelper
+{
+    public function __invoke (SolrMarc $record) : bool
+    {
+        $reader = $record->getMarcReader();
+        if (in_array('Open Access', $reader->getFieldsSubfields('506', ['a']), true)) {
+            return true;
+        }
+        return false;
+    }
+}
